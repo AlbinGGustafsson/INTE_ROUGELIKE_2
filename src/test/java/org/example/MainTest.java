0debead13ccd81dafb18006ab4dc8bdca80d9333
AlbinGGustafsson <abbguu@gmail.com>
2022-10-07 13:35:13 +0200

    Initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+public class MainTest {
+
+    @Test
+    void testExample() {
+        assertEquals(3, Main.testExampleMethod());
+    }
+
+}
