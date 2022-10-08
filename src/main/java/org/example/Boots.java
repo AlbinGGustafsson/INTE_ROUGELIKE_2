17d1a6e4568b98421143f547843137de2b7c4f07
R3Kr <thefungigg@gmail.com>
2022-10-08 18:12:41 +0200

    implementerade Boots klassen

--- src/main/java/org/example/Boots.java
+++ src/main/java/org/example/Boots.java
@@ -1,13 +1,15 @@
 package org.example;
 
 public class Boots extends Armor{
+    private static final int MAX_ARMOR_RATING = 200;
+
     public Boots(String name, String description, int ilvl, int armorRating) {
         super(name, description, ilvl, armorRating);
     }
 
     @Override
     protected int getMaxArmorRating() {
-        return 0;
+        return MAX_ARMOR_RATING;
     }
 
 
