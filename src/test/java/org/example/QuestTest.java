60fc3c39c3b5cf345c36640eb59418b2623344c1
JompasKod <joga9603@student.su.se>
2022-10-11 13:39:29 +0200

    Början på testfall TDD

---
+++ src/test/java/org/example/QuestTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class QuestTest {
+
+    @Test
+    void objectCreatedCorrectly(){
+
+    }
+
+    @Test
+    void newQuestAddedToLog(){
+
+    }
+
+    @Test
+    void completedQuestRemovedFromLog(){
+
+    }
+
+    @Test
+    void objectivesUpdatedCorrectly(){
+
+    }
+}
