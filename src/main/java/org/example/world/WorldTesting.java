a228c8eaf31049a868e0d144c15ab5b3acfbc783
Eloy Lopez <eloyviole@gmail.com>
2022-10-16 14:29:02 +0200

    Fixade lite mer med monster, ändrade lite i combat så att damage är double

--- src/main/java/org/example/world/WorldTesting.java
+++ src/main/java/org/example/world/WorldTesting.java
@@ -1,6 +1,7 @@
 package org.example.world;
 
 import org.example.Helmet;
+import org.example.Monster.Troll;
 import org.example.Player;
 import org.example.Race;
 
@@ -18,13 +19,14 @@ public class WorldTesting {
 
         world.getRoom(0).setEntity(new Stone(), new Position(5,5));
         Player eloy = new Player("Eloy", Race.HUMAN);
-
+        Troll troll = new Troll(10);
         world.spawnPlayer(eloy);
 
 
         eloy.addTerrain(Water.class);
 
         world.getRoom(0).setEntity(eloy, new Position(1,1));
+        world.getRoom(0).setEntity(troll, new Position(3, 4));
         System.out.println(eloy.getPosition());
 
         world.getRoom(0).getTile(new Position(1,0)).addItem(new Helmet("Hjälm", "Skyddar huvudet", 100, 300));
