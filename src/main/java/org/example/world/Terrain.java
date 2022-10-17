f6568ea7ba3075af12095341024baaa251746bf1
AlbinGGustafsson <abbguu@gmail.com>
2022-10-17 15:38:53 +0200

    Smma som den förra men med terrain.... Men T istället för E, inte bestämt mig än men detta känns bättre

--- src/main/java/org/example/world/Terrain.java
+++ src/main/java/org/example/world/Terrain.java
@@ -38,6 +38,8 @@ public abstract class Terrain {
 
     public abstract void printNonReachableMessage();
 
-    public abstract Text getText();
+    public Text getText(){
+        return new Text("T");
+    }
 
 }
