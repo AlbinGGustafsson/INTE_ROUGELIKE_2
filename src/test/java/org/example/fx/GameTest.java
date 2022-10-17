2608260c526842cfd6d0f147093955c0d8bdc7a5
AlbinGGustafsson <abbguu@gmail.com>
2022-10-17 18:07:00 +0200

    Start på test av gui, nu dax för yolo merge med master :D

--- src/test/java/org/example/fx/GameTest.java
+++ src/test/java/org/example/fx/GameTest.java
@@ -3,12 +3,21 @@ package org.example.fx;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 @ExtendWith(ApplicationExtension.class)
@@ -43,7 +52,41 @@ public class GameTest {
 
     @Test
     void test(FxRobot robot) {
+        ComboBox raceComboBox = robot.lookup("#raceComboBox").queryComboBox();
+        ComboBox characterComboBox = robot.lookup("#charComboBox").queryComboBox();
+        TextField nameField = robot.lookup("#nameTextField").queryAs(TextField.class);
+        ColorPicker colorPicker = robot.lookup("#colorPicker").queryAs(ColorPicker.class);
+        Text appearanceText = robot.lookup("#appearanceText").queryText();
+
+        robot.clickOn(nameField).write("playername");
+
+
+        robot.clickOn(raceComboBox).clickOn("ORC");
+        robot.clickOn(characterComboBox);
+
+        for (int i = 0; i < 20; i++){
+            robot.press(KeyCode.DOWN).release(KeyCode.DOWN);
+        }
+
+        robot.clickOn("Q");
+
+        Assertions.assertThat(appearanceText).hasText("Q");
+
+        robot.clickOn(colorPicker);
+
+        for (int i = 0; i < 3; i++){
+            robot.press(KeyCode.DOWN).release(KeyCode.DOWN);
+        }
+
+        for (int i = 0; i < 3; i++){
+            robot.press(KeyCode.RIGHT).release(KeyCode.RIGHT);
+        }
+        robot.press(KeyCode.ENTER);
+
+        assertEquals(colorPicker.getValue(), appearanceText.getFill());
+
         robot.clickOn("#newGameButton");
+
     }
 
 }
