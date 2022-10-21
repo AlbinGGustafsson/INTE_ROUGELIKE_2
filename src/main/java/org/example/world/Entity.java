6f58d059009a8ff4c33f43a47013509317b8b6e1
AlbinGGustafsson <abbguu@gmail.com>
2022-10-21 17:02:11 +0200

    private PrintStream out = new GamePrintStream(); ska inte vara statisk i Entity

--- src/main/java/org/example/world/Entity.java
+++ src/main/java/org/example/world/Entity.java
@@ -6,10 +6,7 @@ import java.io.PrintStream;
 
 public abstract class Entity {
 
-    //TDD gjorde så att jag insåg att det underlättar om Entity är en superklass istället för ett interface.
-    //Den har en metod så man kan byta ut en printstream så att det går att testa.
-    //Alla subklasser har utskrifter så det är najs att ha den metoden i en superklass
-    private static PrintStream out = new GamePrintStream();
+    private PrintStream out = new GamePrintStream();
     private Room room;
     private Position position;
 
