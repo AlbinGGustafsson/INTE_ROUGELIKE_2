6166172a6ba7c6415f3b97b3773678099717d879
AlbinGGustafsson <abbguu@gmail.com>
2022-10-26 16:43:59 +0200

    tog bort kod som inte ska vara där

--- src/main/java/org/example/characters/Player.java
+++ src/main/java/org/example/characters/Player.java
@@ -218,20 +218,10 @@ public class Player extends MovableCharacter implements Combat {
             monster.printNonReachableMessage();
             monster.battleWithPlayer(this);
             tile.removeEntity();
-
         }
 
         if (tile.getEntity() instanceof NPC npc) {
-
             npc.interact(this);
-
-            if (tile.getEntity() instanceof Monster monster) {
-                monster.printNonReachableMessage();
-                monster.battleWithPlayer(this);
-                tile.removeEntity();
-
-            }
-
         }
         return false;
     }
